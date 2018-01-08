d6d77844cc3fa64f633529ef7026b8279a5c2656
R-Girak <roman.girak@gmail.com>
2018-01-08 16:29:20 +0200

    Oracle2

---
+++ src/Chapter01/Ch01_006_ForDemo.java
@@ -0,0 +1,15 @@
+package Chapter01;
+
+/**
+ * Created by Gerka on 08-Jan-18.
+ */
+public class Ch01_006_ForDemo {
+    public static void main(String[] args) {
+        int count;
+
+        for (count = 0; count < 5; count++)
+            System.out.println("Counter value: " + count);
+
+        System.out.println("Done!");
+    }
+}
---
+++ src/Chapter01/Ch01_007_BlockDemo.java
@@ -0,0 +1,19 @@
+package Chapter01;
+
+/**
+ * Created by Gerka on 08-Jan-18.
+ */
+public class Ch01_007_BlockDemo {
+    public static void main(String[] args) {
+        double i, j, d;
+
+        i = 4;
+        j = 10;
+
+        if (i != 0) {
+            System.out.println("i is not equal to 0");
+            d = j / i;
+            System.out.println("j / i is equal to " + d);
+        }
+    }
+}
