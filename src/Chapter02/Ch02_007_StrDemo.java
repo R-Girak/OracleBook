8514dc2df9265e1a857cdc27ff4908be77a32f45
R-Girak <roman.girak@gmail.com>
2018-01-28 21:39:12 +0200

    Oracle2

---
+++ src/Chapter02/Ch02_007_StrDemo.java
@@ -0,0 +1,12 @@
+package Chapter02;
+
+/**
+ * Created by Gerka on 28-Jan-18.
+ */
+public class Ch02_007_StrDemo {
+    public static void main(String[] args) {
+        System.out.println("First line\nSecond line");
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+    }
+}
