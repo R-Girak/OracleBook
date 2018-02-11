d29b828f46fb799dc17f78681f490d8454e2ce60
R-Girak <roman.girak@gmail.com>
2018-02-11 18:28:33 +0200

    home 11-02-2018

---
+++ src/Chapter02/Ch02_008_DynInit.java
@@ -0,0 +1,15 @@
+package Chapter02;
+
+/**
+ * Created by Gerka on 11-Feb-18.
+ */
+public class Ch02_008_DynInit {
+    // Dynamic initialization
+    public static void main(String[] args) {
+        double radius = 4, height = 5;
+
+        double volume = 3.1416 * radius * radius * height;
+
+        System.out.println(volume);
+    }
+}
