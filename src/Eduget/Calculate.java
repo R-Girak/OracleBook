7432c51322911ab93d426d8ac3bdb0fa5f6cda5f
R-Girak <roman.girak@gmail.com>
2018-02-25 21:05:55 +0200

    home 2018-02-25

---
+++ src/Eduget/Calculate.java
@@ -0,0 +1,14 @@
+package Eduget;
+
+public class Calculate {
+
+    private int result;
+
+    public static void main(String[] arg) {
+        System.out.println("Calculate...");
+        int first = Integer.valueOf(arg[0]);
+        int second = Integer.valueOf(arg[1]);
+        int sum = first + second;
+        System.out.println(sum);
+    }
+}
